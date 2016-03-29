6c5c035db2214224ab2f4ff801f0b458f53a8cad
timor2015 <3209742516@qq.com>
2016-03-29 12:32:10 +0800

    添加了项目基础布局设定, 添加了guide展示页面

--- src/scripts/decoration-app.js
+++ src/scripts/decoration-app.js
@@ -1,2 +1,13 @@
 // 引入QApp依赖
 require('QApp');
+
+// 引导展示页
+require('./views/action/guide.js');
+
+
+
+
+// 设定APP首个出现的页面
+QApp.config({
+  indexView: 'guide'
+});
---
+++ src/scripts/views/action/guide.js
@@ -0,0 +1,31 @@
+// 项目展示页
+
+// guide 模板
+var guideTpl = require('../tpl/guide.string');
+
+
+// 使用swiper动作
+require('../lib/swiper.js');
+
+
+
+// 定义guide视图
+QApp.defineView('guide', {
+
+	html : guideTpl,
+
+	plugins : ['delegated'],
+
+	bindActions : {
+
+	},
+
+	bindEvents : {
+		'beforeShow' : function(){
+			var mySwiper = new Swiper ('.swiper-container', {
+			    direction: 'horizontal',
+			    loop: false,
+			});        
+		}
+	}
+});
